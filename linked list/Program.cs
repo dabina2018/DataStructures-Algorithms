40db6c2b7955fdabb84f2251ab4bd02bdb10d6fd
dabina2018 <daveena.cooper18@outlook.com>
2020-04-28 23:17:12 -0700

    started linkedList prob

---
+++ linked list/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace linked_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList myList = new LinkedList();
+            myList.AddAtTail(7);  //0
+            myList.AddAtHead(5); //1
+            myList.AddAtTail(4); //2
+            myList.AddAtTail(22);  //3
+            Console.WriteLine(myList.Get(4));
+        }
+    }
+
+    class Node
+    {
+        public int Value {get; set;}
+        public Node Next {get; set;}
+        public Node(int someVal)
+        {
+            Value = someVal;
+            Next = null;
+        }
+    }
+
+    class LinkedList
+    {
+        Node head;   
+         
+         /** Append a node of value val to the last element of the linked list. */
+        public void AddAtTail(int val) 
+        {
+            Node newNode = new Node (val);
+            Node finger = head;
+
+            if(head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                 while(finger.Next != null)
+                {
+                    finger = finger.Next;
+                }
+                finger.Next = newNode;
+            }
+        }
+
+        /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+        public void AddAtHead(int val) 
+        {
+            Node newNode = new Node(val);
+            //Node finger = head;
+            if(head == null)
+            {
+                head = newNode;
+            }
+            else 
+            {
+                Node temp = head;
+                head = newNode;
+                newNode.Next = temp;
+            }
+        }
+
+        /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+        public int Get(int index) 
+        {
+            Node pointer = head;
+            for (int i = 0; i <= index; i++ )
+            {
+                if(i != index)
+                {
+                    pointer = pointer.Next;
+                }
+            }
+           return pointer.Value;
+        }    
+    
+        /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+        public void AddAtIndex(int index, int val) 
+        {
+        
+        }
+    
+        /** Delete the index-th node in the linked list, if the index is valid. */
+        public void DeleteAtIndex(int index) 
+        {
+        
+        }
+
+    }
+}
