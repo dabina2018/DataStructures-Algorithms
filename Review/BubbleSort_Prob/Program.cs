5df67432b44a3d3c0a01a843e80ac6d873ffc6e8
dabina2018 <daveena.cooper@outlook.com>
2021-01-06 13:19:32 -0600

    Completed Bubble Sort Problem

---
+++ Review/BubbleSort_Prob/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BubbleSort_Prob
+{
+    class Program
+    {
+        //Write a function that takes in an array of integers andreturns a sorted version of that array. 
+        //Use Bubble Sort
+        static void Main(string[] args)
+        {
+            int[] array = { 8, 5, 2, 9, 5, 6, 3 };
+            Console.WriteLine( BubbleSort(array));
+            Console.WriteLine(InsertionSort(array));
+        }
+        public static int[] BubbleSort(int[] array)
+        {
+            // Write your code here.
+            int tmp;
+            int count = 0;
+            while (count < array.Length)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        tmp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = tmp;
+                    }
+                }
+                count += 1;
+            }
+            return array;
+        }
+
+        public static int[] InsertionSort(int[] array)
+        {
+            // Write a function that takes in an array of integers and returns a sorted vesion of that array.
+            //Use insertion sort
+
+
+            return new int[] { };
+        }
+    }
+}
--- Review/ThreeLargestNumbers/Program.cs
+++ Review/ThreeLargestNumbers/Program.cs
@@ -6,45 +6,52 @@ namespace ThreeLargestNumbers
     {
         //Write a function that takes in an array of the at least 3 integers
         //without sorting the input array, return a  sorted array of 3 largest integers
+        //https://stackoverflow.com/questions/30988817/how-to-check-if-int-array-object-is-empty-in-c/30988873#:~:text=int%20is%20a%20non%2Dnullable,use%20of%20a%20nullable%20int.
         static void Main()
         {
-            int[] array = { 55, 7, 8 };
-            Console.WriteLine(  FindThreeLargestNumbers(array));
+            int[] array = { -1, -2, -3, -7, -17, -27, -18, -541, -8, -7, 7 };
+            Console.WriteLine(FindThreeLargestNumbers(array));
         }
-        public static int[] FindThreeLargestNumbers(int[] array)
+        public static int?[] FindThreeLargestNumbers(int[] array)
         {
             // Write your code here.
-            int[] rtrArr = new int[3];
-            for (int i = 0; i < array.Length; i++)
-            {
-                int num = array[i];
-
-
-                if (num > rtrArr[j])
-                {
-                    rtrArr[j] = num;
-                    Sort(rtrArr);
-                    break;
-                }
-                return rtrArr;
-            }
+            int?[] rtrArr = new int?[3];
+            foreach (int num in array)
+                NumShift(rtrArr, num);
             return rtrArr;
         }
-
-        public static int[] Sort(int[] array)
+        static int?[] NumShift(int?[] rtrArray, int num)
         {
-            int tmp = 0;
-            for (int i = 0; i < array.Length-1; i++)
+            if(!rtrArray[2].HasValue || num > rtrArray[2])
             {
-                if (array[i] > array[i + 1])
+                ArrayShifter(rtrArray, num, 2);
+            }
+            else if (!rtrArray[1].HasValue || num > rtrArray[1])
+            {
+                ArrayShifter(rtrArray, num, 1);
+            }
+            else if(!rtrArray[0].HasValue || num > rtrArray[0])
+            {
+                ArrayShifter(rtrArray, num, 0);
+            }
+            return rtrArray;
+        }
+        static int?[] ArrayShifter(int?[] rtrArray, int num, int index)
+        {
+            for (int i = 0; i <= index; i++)
+            {
+                if (i == index)
                 {
-                    tmp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = tmp;
+                    rtrArray[i] = num;
+                }
+                else
+                {
+                    rtrArray[i] = rtrArray[i + 1];
                 }
             }
-            return array;
+            return rtrArray;
         }
+        
     }
 }
 
@@ -71,4 +78,20 @@ namespace ThreeLargestNumbers
                 
             }
             return Sort(rtrArr);
+ */
+/*
+ * public static int[] Sort(int[] array)
+        {
+            int tmp = 0;
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    tmp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tmp;
+                }
+            }
+            return array;
+        }
  */
