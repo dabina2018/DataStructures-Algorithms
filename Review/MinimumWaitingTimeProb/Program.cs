e9155fecc79491a38a556deab4900387dee26e38
dabina2018 <daveena.cooper@outlook.com>
2021-01-22 18:01:17 -0600

    started min waiting time problem.

---
+++ Review/MinimumWaitingTimeProb/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MinimumWaitingTimeProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {3, 2, 1, 2, 6};
+        }
+        public int MinimumWaitingTime(int[] queries)
+        {
+            // Write your code here.
+            return -1;
+        }
+    }
+}
