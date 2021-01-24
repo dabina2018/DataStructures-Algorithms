4f0e6f99ec913600f0c2f9e0feec3b0a91ceb6d3
dabina2018 <daveena.cooper@outlook.com>
2021-01-24 13:22:39 -0600

    not working yet

--- Review/RunLengthEncoding_Prob/Program.cs
+++ Review/RunLengthEncoding_Prob/Program.cs
@@ -4,9 +4,45 @@ namespace RunLengthEncoding_Prob
 {
     class Program
     {
-        static void Main(string[] args)
+        //function takes in a non-empty string and returns its run-length encoding
+        //run-length encoding: a form of losslessdata compression
+        //i.e.: "AAA" == "3A"  AND "AAAAAAAAAA" == "9A3A"
+        //Example: "AAAAAAAAAAAAABBCCCCDD" == 9A4A2B4C2D
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            RunLengthEncoding("AAAAAAAAAAAAABBCCCCDD");
+        }
+
+        public static string RunLengthEncoding(string str)
+        {
+            // Write your code here.
+            int pointer = 1;
+            int counter = 0;
+            string wordmash = "";
+
+
+            if(str[counter] != str[pointer])
+            {
+                if (counter > 0)
+                {
+                    wordmash = wordmash + counter.ToString();
+                }
+                wordmash = wordmash + str[pointer - 1];
+                counter = 0;
+                pointer++;
+                
+            }
+            else
+            {
+                while (str[counter] == str[pointer])
+                {
+                    pointer++;
+                    counter++;
+                }
+                
+            }
+            return wordmash;
         }
     }
+    
 }
