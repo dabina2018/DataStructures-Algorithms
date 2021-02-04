a8381dab7f1d89e0b4db3cab49cbcc73a3f0d8bf
dabina2018 <me@domain.com>
2021-02-04 13:12:26 -0600

    PRAMP: matrix spiral copy

---
+++ Review/LinkedLists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LinkedLists_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node head = new Node(1);
+            head.Next = new Node(2);
+
+        }
+
+        
+    }
+
+    //Add at Tail
+    //Add at Head
+    //GetValue at nth index
+    //Get Len
+    //Add at Index
+    //Delete at Index
+    //Reverse the list
+    //Print the list
+    class LinkedList
+    {
+        Node head;
+        public static void PrintList(Node head)
+        {
+
+        }
+    }
+    
+    class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public Node(int someVal)
+        {
+            Value = someVal;
+            Next = null;
+        }
+    }
+}
---
+++ Review/MatrixSpiralCopy/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace MatrixSpiralCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            int[,] array = new int[5, 4];
+            Console.WriteLine(array.GetLength(0));
+            Console.WriteLine(array.GetLength(1));
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    array[i, j] = counter;
+                }
+                
+            }
+        }
+        public static int[] SpiralCopy(int[,] inputMatrix) //int[row,column]
+        {
+            // your code goes here
+
+            int right2Lef = inputMatrix.GetLength(0);//length of row
+            int left2R = 0;
+            int top2bot = inputMatrix.GetLength(1); // length of column
+            int bot2top = 0;
+            int counter = 0;
+            int[] rtrArr = new int[inputMatrix.Length];
+
+            for (int i = 0; i < inputMatrix.GetLength(0); i++)//i=row
+            {
+                for (int j = 0; j < inputMatrix.GetLength(1); j++) //j=column
+                {
+                    //iterate thru row from R to L and add values
+                    while (j < right2Lef)
+                    {
+                        rtrArr[counter] = inputMatrix[i, j];
+                        counter++;
+                        j++;
+                    }
+                    //iterate thru column top to bottom
+                    while (i < top2bot)
+                    {
+                        rtrArr[counter] = inputMatrix[i, j];
+                        counter++;
+                        i++;
+                    }
+                    //iterate thru row from L to R and add values
+                    while (j >= left2R)
+                    {
+                        rtrArr[counter] = inputMatrix[i, j];
+                        counter++;
+                        j--;
+                    }
+                    //iterate thru column from bottom to top
+                    while (i > bot2top)
+                    {
+                        rtrArr[counter] = inputMatrix[i, j];
+                        counter++;
+                        i--;
+                    }
+                    right2Lef--;
+                    left2R++;
+                    top2bot--;
+                    bot2top++;
+                }
+            }
+            return rtrArr;
+        }
+    }
+}
