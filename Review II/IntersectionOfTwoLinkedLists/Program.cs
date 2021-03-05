a51c0dd69debd66937e5818b3942f53e61affdf0
dabina2018 <dabina2018@me.com>
2021-03-05 16:15:29 -0600

    completed daily challenges for march contest

---
+++ Review II/IntersectionOfTwoLinkedLists/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IntersectionOfTwoLinkedLists
+{
+    class Program
+    {
+        //Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+        //Output: Reference of the node with value = 8
+        //Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). 
+        //      From the head of A, it reads as [4,1,8,4,5]. 
+        //      From the head of B, it reads as [5,6,1,8,4,5]. 
+        //There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+        static void Main()
+        {
+            ListNode listNodeA = new ListNode(4);
+            listNodeA.next = new ListNode(1);
+            listNodeA.next.next = new ListNode(8);
+            listNodeA.next.next.next = new ListNode(4);
+            listNodeA.next.next.next.next = new ListNode(5);
+
+            ListNode listNodeB = new ListNode(5);
+            listNodeB.next = new ListNode(6);
+            listNodeB.next.next = new ListNode(1);
+            listNodeB.next.next.next = new ListNode(8);
+            listNodeB.next.next.next.next = new ListNode(4);
+            listNodeB.next.next.next.next.next = new ListNode(5);
+            GetIntersectionNode(listNodeA, listNodeB);
+        }
+       
+        public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            HashSet<ListNode> listA = new HashSet<ListNode>();
+                
+            ListNode current = headA;
+            while (current.next != null)
+            {
+                listA.Add(current);
+                current = current.next;
+            }
+            current = headB;
+            while (current.next != null)
+            {
+                if (listA.Contains(current)) //check for matching node
+                {
+                        return current; //check hashset at found key for matching node
+                }
+                current = current.next;
+            }
+            return null;
+        }
+        
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        //traverse listA and add each node to a hashtable (val/next)
+        // traverse list B and compare each node to hashtable entries
+        //          return the match
+
+    }
+}
--- Review II/MissingNumber/Program.cs
+++ Review II/MissingNumber/Program.cs
@@ -11,7 +11,7 @@ namespace MissingNumber
         //  and O(n) runtime complexity?
         static void Main()
         {
-            int[] array = new int[] {9, 6, 4, 2, 3, 5, 7, 0, 1};
+            int[] array = new int[] { 0, 1};
             Console.WriteLine(MissingNumber(array));
         }
         public static int MissingNumber(int[] nums)
@@ -32,10 +32,28 @@ namespace MissingNumber
                     }
                 }
             }
-            
-            return max;
-            
-           
+            return max + 1;
+        }
+
+        public static int MissingNumberII(int[] nums)
+        {
+            int length = nums.Length;
+            var seen = new HashSet<int>();
+
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!seen.Contains(nums[i]))
+                    seen.Add(nums[i]);
+            }
+
+            for (int i = 0; i <= nums.Length; i++)
+            {
+                if (!seen.Contains(i))
+                    return i;
+            }
+
+            return 0;
         }
     }
 }
