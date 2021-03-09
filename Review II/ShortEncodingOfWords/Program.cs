8ad24b10a3d085a0e5b7b334fe954a564d239f91
dabina2018 <dabina2018@me.com>
2021-03-09 15:03:33 -0600

    working thru encoding problem

--- Review II/ShortEncodingOfWords/Program.cs
+++ Review II/ShortEncodingOfWords/Program.cs
@@ -7,29 +7,47 @@ namespace ShortEncodingOfWords
     {
         static void Main()
         {
-            string[] array = new string[] { "time", "me", "bell" };
+            string[] array = new string[] {"esquire", "e"};
             MinimumLengthEncoding(array);
         }
         public static int MinimumLengthEncoding(string[] words)
         {
             string s = "";
             List<string> wordz = new List<string>(words);
-            int i = 0;
-            
-            if (wordz[i] != wordz[i + 1])
+            //int i = 0;
+            if (words.Length != 1)
             {
-                if (wordz[i].Contains(wordz[i + 1]))
+                foreach (string wd in words)
                 {
-                    wordz.Remove(wordz[i+1]);
+                    for (int i = 1; i < words.Length; i++)
+                    {
+                        wordz.Remove(wd.Substring(i));  
+                    }
                 }
             }
-
             foreach (string word in wordz)
             {
                 s = s + word + "#";
             }
 
             return s.Length;
+        }
+
+        public static int MinimumLengthEncodingII(string[] words)
+        {
+            HashSet<String> good = new HashSet<string>(words);
+            foreach (String word in words)
+            {
+                for (int k = 1; k < word.Length; ++k)
+                {
+                    good.Remove(word.Substring(k));
+                }
+            }
+
+            int ans = 0;
+            foreach (String word in good)
+                ans += word.Length + 1;
+            return ans;
         }
     }
 }
