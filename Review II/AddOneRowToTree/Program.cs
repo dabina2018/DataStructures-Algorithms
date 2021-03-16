3ee0c20560cabb3ac57e8e792d35364825fd84f6
dabina2018 <dabina2018@me.com>
2021-03-16 12:08:29 -0500

    updated Add Tree Row Prob

--- Review II/AddOneRowToTree/Program.cs
+++ Review II/AddOneRowToTree/Program.cs
@@ -1,12 +1,102 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace AddOneRowToTree
 {
     class Program
     {
-        static void Main(string[] args)
+        //Given the root of a binary tree and two integers val and depth
+        //add a row of nodes with value val at the given depth depth.
+        //Note that the root node is at depth 1.
+        //The adding rule is:
+                //Given the integer depth, for each not null tree node cur at the depth depth - 1,
+                //create two tree nodes with value val as cur's left subtree root and right subtree root.
+                //cur's original left subtree should be the left subtree of the new left subtree root.        
+                //cur's original right subtree should be the right subtree of the new right subtree root.
+                //If depth == 1 that means there is no depth depth - 1 at all,
+                //then create a tree node with value val as the new root of the whole original tree,
+                //and the original tree is the new root's left subtree.
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            TreeNode root = new TreeNode(4);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(6);
+
+            root.left.left = new TreeNode(3);
+            root.left.right = new TreeNode(1);
+
+            root.right.left = new TreeNode(5);
+            PrintTree(root);
+       
         }
+        public TreeNode AddOneRow(TreeNode root, int val, int depth)
+        {
+            TreeNode newNode = new TreeNode(val);
+            TreeNode current = root;
+            int currDepth = depth;
+
+
+            while (current != null && currDepth < depth)
+            {
+                current = current.left;
+                depth += 1;
+            }
+            newNode.left = current.left;
+            newNode.right = null;
+            current.left = newNode;
+            while (current != null && currDepth < depth)
+            {
+                current = current.right;
+                depth += 1;
+            }
+            newNode.right = current.right;
+            newNode.left = null;
+            current.right = newNode;
+            return root;
+        }
+        public void TraverseTree(TreeNode root, int currDepth, int desDepth)
+        {
+            int Depth = currDepth;
+            if (root!= null && currDepth < desDepth)
+            {
+                TraverseTree(root.left, currDepth + 1, desDepth);
+                TraverseTree(root.right, currDepth + 1, desDepth);
+            }
+        }
+        
+
+        public static void PrintTree(TreeNode root)
+        {
+            if (root != null)
+            {
+                Console.WriteLine(root.val);
+                PrintTree(root.left);
+                PrintTree(root.right);
+            }
+        }
+
+       
+
+       
+        //--------------------------
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+      }
     }
 }
+
+//Input: root = [4,2,6,3,1,5], val = 1, depth = 2
+//Output:[4,1,1,2,null,null,6,3,1,5]
+
+//Input: root = [4,2,null,3,1], val = 1, depth = 3
+//Output:[4,2,null,1,1,3,null,null,1]
---
+++ Review II/TwoSum_Revisited/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace TwoSum_Revisited
+{
+    // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    // You can return the answer in any order.
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] rtrArr = new int[10];
+
+            return rtrArr;
+        }
+
+    }
+}
+
+//Example 1:
+//Input: nums = [2, 7, 11, 15], target = 9
+//Output:[0,1]
+//Output: Because nums[0] +nums[1] == 9, we return [0, 1].
