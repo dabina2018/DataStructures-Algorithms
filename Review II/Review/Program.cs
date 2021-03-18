ebf0c9719402db0d85bb5db922168671d51f15cf
dabina2018 <dabina2018@me.com>
2021-03-17 23:50:00 -0500

    almost there

--- Review II/Review/Program.cs
+++ Review II/Review/Program.cs
@@ -7,20 +7,22 @@ namespace Review
     {
         static void Main(string[] args)
         {
-            IntToRoman(1994);
+            //IntToRoman(58);
+           // IntToRoman(1994);
+            IntToRoman(3);
         }
         public static string IntToRoman(int num)
         {
             //create a hashset of roman numerals and thier num equivelant
-            Hashtable ht = new Hashtable() { { "I", 1 }, { "V", 5 }, { "X", 10 }, { "L", 50 }, { "C", 100 }, { "D", 500 }, { "M", 1000 } };
-            Hashtable htt = new Hashtable() { { "IV", 4 }, { "IX", 9 }, { "XL", 40 }, { "XC", 90 }, { "CD", 400 }, { "CM", 900 } };
+            Hashtable ht = new Hashtable() { { "I", 1 }, { "V", 5 }, { "X", 10 }, { "L", 50 }, { "C", 100 }, 
+                { "D", 500 }, { "M", 1000 }, { "IV", 4 }, { "IX", 9 }, { "XL", 40 }, { "XC", 90 }, { "CD", 400 }, { "CM", 900 } };
+            
 
             //divide the input number by value of each dictionary entry
             int val = num;
             string rtr = "";
             int div = 1000;
             int tmp = 0;
-           
 
             while (div > 0)
             {
@@ -32,32 +34,30 @@ namespace Review
                 {
                     tmp = (val / div); // add var for divisor
                     val = val % div;
-                    if (div*10 - tmp*div == div || div*5 - tmp*div == div)
+                   
+                    foreach (object key in ht.Keys)
                     {
-                        foreach (object key in htt.Keys)
+                        if (tmp * div == (int)ht[key])
                         {
-                            if (tmp * div == (int)htt[key])
-                            {
-                                rtr = rtr + key;                        // add var for string
-                                break;
-                            }
+                            rtr = rtr + key;         // add var for string
+                            break;
                         }
                     }
-                    else
+                    if (tmp < 10)
                     {
-                        int diff = 0;
-                        while (diff != tmp)
+                        if (tmp / 5 == 1)
                         {
-                            foreach (object key in ht.Keys)
-                            {
-                                if (diff + (int)ht[key] <= tmp)
-                                {
-                                    diff = diff + (int)ht[key];
-                                    rtr = rtr + key;
-                                }
-                            }
-                        }                   // add var for string
+                            tmp -= 5;
+                            rtr = rtr + 'V';
+                        }
+
+                        for (int i = 0; i < tmp; i++)
+                        {
+                            rtr = rtr + 'I';
+                        }
                     }
+                    
+
                     div = div / 10;
                 }
             }
