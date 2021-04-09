34502307514b297c7d7679e7c739dd57020b64ac
dabina2018 <dabina2018@me.com>
2021-04-09 12:11:44 -0700

    adding changes

--- Review II/BinaryString/Program.cs
+++ Review II/BinaryString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BinaryString
 {
@@ -13,12 +14,50 @@ namespace BinaryString
             int k = 2;
             HasAllCodes(s, k);
         }
+        static HashSet<string> cache = new();
+        static char[] options = { '0', '1' };
         public static bool HasAllCodes(string s, int k)
         {
             // find all permutations for len 'k'
             // see if string contains each permutation
-
-
+            string str = "";
+            bool rtr = true;
+           
+            Helper(str, k);
+                     
+            foreach (string permutation in cache)
+            {
+                if (s.Contains(permutation))
+                {
+                    rtr = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return rtr;
+        }
+        public static void Helper(string str, int k)
+        {
+            foreach (char option in options)
+            {
+                str = str + option;
+                foreach (char option2 in options)
+                {
+                    str = str + option2;
+                    if (str.Length == k)
+                    {
+                        cache.Add(str);
+                        //break;
+                    }
+                    else if (str.Length < k)
+                    {
+                        Helper(str, k);
+                    }
+                }
+               
+            }
         }
     }
 }
