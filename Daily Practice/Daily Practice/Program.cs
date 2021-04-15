2145ebaad486da0863286b9034e04c42885dec6b
dabina2018 <daveena.cooper18@outlook.com>
2021-04-15 10:35:01 -0700

    adding daily practice project

---
+++ Daily Practice/Daily Practice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Daily_Practice
+{
+    class Program
+    {
+        //Given an array nums.
+        ////We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+        ///Return the running sum of nums.
+        static void Main(string[] args)
+        {
+            int[] input1 = { 1, 2, 3, 4 };
+            int[] input2 = { 1, 1,1,1 };
+            int[] input3 = { 1, 3,1,2,10,1 };
+            Console.WriteLine(RunningSum(input1));
+        }
+        
+        static public int[] RunningSum(int[] nums)
+        {
+            int[] rtrArr = new int[nums.Length];
+            // iterate thru each index in the array
+            // add the current index to the next index
+            // update the next index to be equal to the sum
+            rtrArr[0] = nums[0];
+            
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int j = 0;
+                int sum = 0;
+                while (j <= i)
+                {
+                    sum = sum + nums[j];
+                    j++;
+                }
+                rtrArr[i] = sum;
+            }
+            return nums;
+        }
+        
+    }
+}
--- Review II/TwoSum_Revisited/Program.cs
+++ Review II/TwoSum_Revisited/Program.cs
@@ -46,7 +46,37 @@ namespace TwoSum_Revisited
             
             return rtrArr;
         }
-
+        static public int[] BasicSort(int[] nums)
+        {
+            return new int[] { };
+        }
+        static public int[] SelectionSort(int[] nums)
+        {
+            //find the minimum elemnt, place it at the beginning
+            //repeat
+            return new int[] { };
+        }
+        static public int[] BubbleSort(int[] nums)
+        {
+            //swap adjacent numbers
+            //repeat
+            return new int[] { };
+        }
+        static public int[] QuickSort(int[] nums)
+        {
+            //pick pivot index
+            // place anything smaller to the left
+            // place anythign greater to the right
+            //repeat (on each half of arr from pivot)
+            return new int[] { };
+        }
+        static public int[] MergeSort(int[] nums)
+        {
+            // from midpoint divide array into half
+            // repeat until each 'half' len == 1
+            // merge each half back together in correct order
+            return new int[] { };
+        }
     }
 }
 
